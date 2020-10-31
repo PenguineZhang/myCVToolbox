1a0e52ab42d2acd1d419c9e81a98d799ca3b418e
Peng Zhang <pzhang@spawar.navy.mil>
2020-10-30 18:42:15 -0700

    worked on corner detector

--- CornerDetector.py
+++ CornerDetector.py
@@ -1,20 +1,66 @@
-#!/home/peng/anaconda3/envs/fcnd/bin/python
-
 from Exceptions import *
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
 import cv2
+import time
 
 class CornerDetection(object):
     def __init__(self):
         pass
 
-    def corners(self, img: np.ndarray, algorithm='harris'):
+    def corners(self, img: np.ndarray, algorithm='harris', w='box'):
         '''
         input: img - grayscale image needed to find corners
                algorithm - ['harris', 'forstner']
+               w - ['gaussian', 'box']
         '''
         if len(img.shape) > 2:
             raise NotGrayscaleImageError()
+
+        available_w = ['box', 'gaussian']
+
+        filter = None
+        if w in available_w:
+            if w == 'box':
+                filter = self.box2dGeneration()
+            elif w == 'gaussian':
+                filter = self.gaussian2dGeneration()
+        else:
+            # TODO: raise exception here
+            raise FilterUnavailableError()
+
+        # kernel for I_x
+        kernel_x = 1/12 * np.array([[-1, 8, 0, -8, 1]])
+
+        # kernel for I_y
+        kernel_y = np.transpose(kernel_x)
+
+        # calculate gradient map in X direction
+        I_x = scipy.signal.convolve2d(img, kernel_x)
+
+        # calculate gradient map in Y direction
+        I_y = scipy.signal.convolve2d(img, kernel_y)
+
+        # precompute I_x^2, I_y^2, I_x*I_y
+        I_x2 = I_x**2
+        I_y2 = I_y**2
+        I_xy = I_x*I_y
+
+        filter_size = filter.shape
+        h, w = img.shape
+
+        row_start = filter_size[0]//2
+        row_end = h - filter_size[0]//2
+        col_start = filter_size[1]//2
+        col_end = h - filter_size[1]//2
+
+        corners = []
+        for i in range(row_start, row_end):
+            for j in range(col_start, col_end):
+                Ix2 = np.sum(I_x2[i-filter_size[0]:i+filter_size[0], j-filter_size[1]:j+filter_size[1]] * filter)
+                Iy2 = np.sum(I_y2[i-filter_size[0]:i+filter_size[0], j-filter_size[1]:j+filter_size[1]] * filter)
+                Ixy = np.sum(I_xy[i-filter_size[0]:i+filter_size[0], j-filter_size[1]:j+filter_size[1]] * filter)
 
 
         return 1
@@ -22,6 +68,48 @@ class CornerDetection(object):
     def matchCorners(self, image1, image2):
         pass
 
+
+    @staticmethod
+    def gaussian2dGeneration(dim=[3,3]):
+
+        if len(dim) > 2:
+            # TODO: make this an exception
+            print("Dimension must be 2d")
+
+        if dim[0] != dim[1]:
+            # TODO: make this an exception
+            print("Must be square matrix")
+
+        sigma_x = 1
+        sigma_y = 1
+        theta = 0
+
+        X, Y = np.meshgrid(range(-dim[0], dim[0]+1), range(-dim[0], dim[0]+1))
+
+        A = 1
+        a = np.cos(theta)**2 / (2 * sigma_x**2) + np.sin(theta)**2 / (2 * sigma_y**2)
+        b = -np.sin(2 * theta) / (4 * sigma_x**2) + np.sin(2 * theta) / (4 * sigma_y**2)
+        c = np.sin(theta)**2 / (2 * sigma_x**2) + np.cos(theta)**2 / (2 * sigma_y**2)
+
+        return A * np.exp( -(a * X**2 + 2 * b * X * Y + c * Y**2))
+
+    @staticmethod
+    def box2dGeneration(dim=[3,3]):
+
+        if len(dim) > 2:
+            # TODO: make this an exception
+            print("Dimension must be 2d")
+
+        if dim[0] != dim[1]:
+            # TODO: make this an exception
+            print("Must be square matrix")
+
+        h, w = dim[0], dim[1]
+        box_filter = 1 / (h*w) * np.ones((h, w))
+        return box_filter
+
+
 corner_detector = CornerDetection()
-fake_data = np.random.randn(30,30,3)
-corners = corner_detector.corners(fake_data)
+img = cv2.imread("test.png", 0)
+print(img.shape)
+corners = corner_detector.corners(img)
--- Exceptions.py
+++ Exceptions.py
@@ -2,3 +2,9 @@ class NotGrayscaleImageError(Exception):
     def __init__(self, message="Image must be a grayscale image"):
         self.message = message
         super().__init__(self.message)
+
+
+class FilterUnavailableError(Exception):
+    def __init__(self, message="Specified filter is unavailable"):
+        self.message = message
+        super().__init__(self.message)
